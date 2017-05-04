2d8100e3961abb6ef83b472221084f87b8e3efa3
Chenshiwen <cswzmg@live.com>
2017-05-04 16:46:29 +0800

    Initial Commit

---
+++ OpenGLES  01Tests/OpenGLES__01Tests.m
@@ -0,0 +1,39 @@
+//
+//  OpenGLES__01Tests.m
+//  OpenGLES  01Tests
+//
+//  Created by ShiWen on 2017/5/4.
+//  Copyright © 2017年 ShiWen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OpenGLES__01Tests : XCTestCase
+
+@end
+
+@implementation OpenGLES__01Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
